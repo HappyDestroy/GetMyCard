be1ae2ed47db1548aaabb57b5d93567647f5ad56
frodonic <nicolassaboureau@gmail.com>
2014-05-07 16:11:35 +0200

    Pivot !

--- GetMyCard/Views/MaCarte.xaml.cs
+++ GetMyCard/Views/MaCarte.xaml.cs
@@ -7,14 +7,24 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
 
 namespace GetMyCard
 {
     public partial class MaCarte : PhoneApplicationPage
     {
+        #region Fields
+        PhotoChooserTask photoChooserTask;
+        #endregion
         public MaCarte()
         {
+            photoChooserTask = new PhotoChooserTask();
+            photoChooserTask.Completed += new EventHandler<PhotoResult>(photoChooserTask_Completed);
             InitializeComponent();
         }
+
+
+
+        public EventHandler<PhotoResult> photoChooserTask_Completed { get; set; }
     }
 }
